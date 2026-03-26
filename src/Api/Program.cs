c3e1b4805c894e795fc2171818b532bce9d68a6e
Sergii Shvets <sergius.shvets@gmail.com>
2026-03-26 17:12:46 -0400

    added Identity Server

---
+++ src/Api/Program.cs
@@ -0,0 +1,36 @@
+using System.Security.Claims;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddControllers();
+
+builder.Services.AddAuthentication()
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "https://localhost:5001";
+        options.TokenValidationParameters.ValidateAudience = false;
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "api1");
+    });
+});
+
+// Add services to the container.
+
+var app = builder.Build();
+// app.MapControllers();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.MapGet("identity", (ClaimsPrincipal user) => user.Claims.Select(c => new { c.Type, c.Value }))
+    .RequireAuthorization("ApiScope");
+
+app.Run();
+
